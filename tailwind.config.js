4e522d01907ed4fe759e16c95c65d7daacfcea2a
jahidanowar <milon5143@gmail.com>
2023-02-24 00:05:39 +0530

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
+    "components/**/*.vue",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "plugins/**/*.js",
   ],
   theme: {
     extend: {
