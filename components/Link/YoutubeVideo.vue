5e7ad3039bc861088e29af9846f8c0b55ebae252
jahidanowar <milon5143@gmail.com>
2023-10-24 08:15:46 +0530

    style: Gradient border added to the youtube link

--- components/Link/YoutubeVideo.vue
+++ components/Link/YoutubeVideo.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <a
     :href="`https://youtube.com/watch?v=${video.id.videoId}`"
-    class="glass flex flex-col lg:flex-row rounded-xl overflow-hidden items-center group"
+    class="youtube-video gradient-box flex flex-col lg:flex-row items-center group rounded-xl"
   >
     <div class="relative aspect-video w-full lg:w-auto lg:h-[90px]">
       <NuxtImg
@@ -16,7 +16,7 @@ defineProps<{
         width="256"
         height="144"
         fit="cover"
-        class="w-full h-full absolute group-hover:scale-105 transition-all duration-300"
+        class="w-full h-full absolute transition-all duration-300 rounded-t-xl lg:rounded-t-none lg:rounded-l-xl"
       ></NuxtImg>
     </div>
 
@@ -32,4 +32,52 @@ defineProps<{
   </a>
 </template>
 
-<style></style>
+<style scoped>
+.gradient-box {
+  @apply bg-white dark:bg-gray-800;
+  position: relative;
+  box-sizing: border-box;
+  background-clip: padding-box;
+}
+.gradient-box:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  margin: -2px;
+  /* !importanté */
+  border-radius: 14px;
+  /* !importanté */
+  background: linear-gradient(
+    to right,
+    hsl(100 100% 60%),
+    hsl(200 100% 60%),
+    hsl(300 100% 60%),
+    hsl(400 100% 60%),
+    hsl(500 100% 60%),
+    hsl(600 100% 60%),
+    hsl(700 100% 60%),
+    hsl(800 100% 60%),
+    hsl(900 100% 60%),
+    hsl(1000 100% 60%)
+  );
+  background-size: 400% 400%;
+  transition: all 0.2s ease-in-out;
+  animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
